a03fceba8e4f81eca7a62399e01f26cf3ef745c6
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-04-21 14:35:49 -0400

    install type

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
-const express = require('express');
+import express from 'express';
+// require('dotenv').config();
+// const express = require('express');
 const cors = require('cors');
 const { Configuration, OpenAIApi } = require('openai');
 
